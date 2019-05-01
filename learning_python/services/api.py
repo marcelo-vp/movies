6d33254db19a7fe412f57a576c3b80acc1f9daec
marcelo-vp <marcelos.mail@gmail.com>
2019-05-01 14:22:28 -0300

    Enhancing get_movie_data function with regex and docs

--- learning_python/services/api.py
+++ learning_python/services/api.py
@@ -1,14 +1,31 @@
-import requests
+import re, requests
 from learning_python.helpers.constants import OMDB_BASE_URL
 from learning_python.settings import OMDB_API_KEY
 
 
-def get_movie_data(title, plot='full'):
+def get_movie_data(title, **kwargs):
+    """ Gets movie data from OMDb
+
+    Required parameters:
+        title(str): movie title
+
+    Optional parameters:
+        type(str): movie, series, episode
+        y(num): year of release
+        plot(str): short(default) or full
+        r(str): return data - json(default) or xml
+
+    Returns:
+        movie_data(dict): movie data in JSON format
+
+    """
     omdb_base_url = OMDB_BASE_URL
+    title = re.sub('\\s+', '+', title)
     params = {
         'apikey': OMDB_API_KEY,
-        't': title,
-        'plot': plot
+        't': title
     }
+    for key, value in kwargs.items():
+        params[key] = str(value)
     movie_data = requests.get(omdb_base_url, params=params)
     return movie_data.json()

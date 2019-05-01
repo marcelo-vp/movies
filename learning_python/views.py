12056964333bfea1f3bf66abde58748c89a81dec
marcelo-vp <marcelos.mail@gmail.com>
2019-05-01 14:24:37 -0300

    Simplifying view input and showing only movie info to user

--- learning_python/views.py
+++ learning_python/views.py
@@ -1,15 +1,10 @@
 import sys
 from flask import render_template
 from learning_python import app
-from learning_python.helpers.expressions import aboutTesla
 from learning_python.services.api import get_movie_data
 
 @app.route('/')
 def index():
-    movie_name = 'blade runner 2049'.replace(' ', '+')
-    movie_data = get_movie_data(movie_name)
-    return render_template('index.html', intro=intro, movie_data=movie_data)
-
-intro = ['Python running on: %s' % sys.platform]
-intro.append(aboutTesla)
-intro.append('CEO profile:')
+    movie_name = 'blade runner 2049'
+    movie_data = get_movie_data(movie_name, plot='full')
+    return render_template('index.html', movie_data=movie_data)

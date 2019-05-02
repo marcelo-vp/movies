cca20119f2c5cf5171dedb91124d915a611e447e
marcelo-vp <marcelos.mail@gmail.com>
2019-05-01 22:11:15 -0300

    Handling value and request errors separately

--- learning_python/services/api.py
+++ learning_python/services/api.py
@@ -3,6 +3,14 @@ from learning_python.helpers.constants import OMDB_BASE_URL
 from learning_python.settings import OMDB_API_KEY
 
 
+def get_response_as_boolean(data):
+    response_as_boolean = True
+    response_as_string = data['Response']
+    if response_as_string == 'False':
+        response_as_boolean = False
+    return response_as_boolean
+
+
 def get_movie_data(title, **kwargs):
     """ Gets movie data from OMDb
 
@@ -27,12 +35,26 @@ def get_movie_data(title, **kwargs):
     }
     for key, value in kwargs.items():
         params[key] = str(value)
-    movie_data = requests.get(omdb_base_url, params=params).json()
+
+    # Tries to get a response within the timeout value
+    # and with status code == 200
     try:
-        if not eval(movie_data['Response']):
-            raise ValueError('Movie not found!')
-    except ValueError as error:
+        response = requests.get(omdb_base_url, params=params, timeout=3.00)
+        if not response.status_code == 200:
+            response.raise_for_status()
+    except Exception as error:
         print(error)
-        return {'title': 'Not found'}
+        return 'Service unavailable. Try again later.'
     else:
-        return movie_data
+        # Tries to parse the response into JSON format
+        # and checks if the "Response" key is not False
+        try:
+            movie_data = response.json()
+            has_response = get_response_as_boolean(movie_data)
+            if not has_response:
+                raise ValueError('Movie not found!')
+        except ValueError as error:
+            print(error)
+            return 'Movie has invalid data or was not found.'
+        else:
+            return movie_data

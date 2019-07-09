9b54b0db25f409aefab4eb34b1472d2b9e08a6b4
marcelo-vp <marcelos.mail@gmail.com>
2019-07-09 15:28:08 -0300

    Adding React and Babel

--- movies/static/src/index.js
+++ movies/static/src/index.js
@@ -1,5 +1,14 @@
-import handleMovieSearch from './omdb'
-import { handleAddFavorite, handleGetFavorites } from './favorites'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import handleMovieSearch from './omdb';
+import { handleAddFavorite, handleGetFavorites } from './favorites';
+
+function Greetings(props) {
+    return <p>{props.userName}</p>;
+}
+
+ReactDOM.render(<Greetings userName="Marcelo Pinto"/>, document.querySelector('.greetings'));
 
 document.addEventListener('DOMContentLoaded', () => {
     handleMovieSearch();
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,23 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'movies/static/dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react'
+                        ],
+                        cacheDirectory: true
+                    }
+                }
+            }
+        ]
     }
 };

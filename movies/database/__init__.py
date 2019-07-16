b4d8175dfe62707916db3c9486e62a50e24dac04
marcelo-vp <marcelos.mail@gmail.com>
2019-07-16 19:06:55 -0300

    Transforming the MoviesDB into a base class

--- movies/database/__init__.py
+++ movies/database/__init__.py
@@ -6,20 +6,9 @@ class MoviesDB:
 
     def __init__(self):
         self.db = self.db_client.movies_app
-        self.favorites = self.db.favorites
 
     @property
     def db_client(self):
         if not self.client:
             self.client = MongoClient()
         return self.client
-
-    def add_favorite(self, data):
-        self.favorites.insert_one(data)
-
-    def list_favorites(self):
-        favorites = []
-        for favorite in self.favorites.find():
-            del favorite['_id']
-            favorites.append(favorite)
-        return favorites

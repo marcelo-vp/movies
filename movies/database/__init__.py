a0fc7ac7e765e995bfbf8e32c677155ac43b3232
marcelo-vp <marcelos.mail@gmail.com>
2019-07-17 23:04:23 -0300

    Improving movies db base class with built-in function

--- movies/database/__init__.py
+++ movies/database/__init__.py
@@ -2,13 +2,11 @@ from pymongo import MongoClient
 
 
 class MoviesDB:
-    client = None
-
     def __init__(self):
         self.db = self.db_client.movies_app
 
     @property
     def db_client(self):
-        if not self.client:
+        if not hasattr(self, 'client'):
             self.client = MongoClient()
         return self.client

dc4ac6e1f3a61019c76818fedf22d99592cf9483
marcelo-vp <marcelos.mail@gmail.com>
2019-07-18 00:56:30 -0300

    Added a custom MUI theme

---
+++ movies/static/src/theme/index.js
@@ -0,0 +1,9 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const palette = {
+    primary: { main: '#E65100' },
+    secondary: { main: '#212121' }
+};
+const themeName = 'Trinidad Mine Shaft Eagle';
+
+export default createMuiTheme({ palette, themeName });

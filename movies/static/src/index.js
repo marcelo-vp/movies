392075e4a71c0f488a77fb503d4361793ccd4b4d
marcelo-vp <marcelos.mail@gmail.com>
2019-08-19 22:41:36 -0300

    Managing core-js and regenerator-runtime dependencies

--- movies/static/src/index.js
+++ movies/static/src/index.js
@@ -1,8 +1,3 @@
-import 'core-js-pure/stable'
-import 'regenerator-runtime/runtime';
-// Try to remove the imports above, since they
-// are only required for async/await to work
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';

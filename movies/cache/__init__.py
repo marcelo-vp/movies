7d7e8945105542a09d799af50f516841da034284
marcelo-vp <marcelos.mail@gmail.com>
2019-09-04 09:09:28 -0300

    Updates Cache implementation so that there is only one Redis connection

--- movies/cache/__init__.py
+++ movies/cache/__init__.py
@@ -2,9 +2,7 @@ import redis
 
 
 class Cache:
-    def __init__(self):
-        if not hasattr(self, 'client'):
-            self.client = redis.Redis()
+    client = redis.Redis()
 
     def set_value(self, key, value):
         self.client.set(key, value)

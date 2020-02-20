b92893dc5a53200e50f06e9a4e5862b000ef20ac
marcelo-vp <marcelos.mail@gmail.com>
2020-02-20 00:28:52 -0300

    Adds default database and cache settings

--- movies/settings.py
+++ movies/settings.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 OMDB_API_KEY = os.getenv('OMDB_API_KEY')
-REDIS_URL = os.environ.get('REDIS_URL')
-DATABASE = os.environ.get('DATABASE')
-MONGO_URL = os.environ.get('MONGO_URL')
+REDIS_URL = os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379')
+DATABASE = os.environ.get('DATABASE', 'mongo')
+MONGO_URL = os.environ.get('MONGO_URL', 'mongodb://127.0.0.1:27017/')
